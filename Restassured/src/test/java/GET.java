7df41137e363da3cfd266658d3189d846be43d27
shhweta <56310793+shhweta@users.noreply.github.com>
2020-04-06 17:36:00 +0530

    Add files via upload

---
+++ Restassured/src/test/java/GET.java
@@ -0,0 +1,44 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+
+import  org.apache.commons.lang3.builder.EqualsBuilder;
+import static org.hamcrest.Matchers.*;
+
+
+public class GET {
+	
+	@Test
+	void test_1()
+	{
+	Response response = get("https://reqres.in/api/users?page=2");
+	
+	 System.out.println(response.asString());
+	 System.out.println(response.getBody());
+	 System.out.println(response.getStatusCode());
+	 System.out.println(response.getStatusLine());
+	 System.out.println(response.getHeader("Content-Type"));
+	 System.out.println(response.getTime());
+	 
+	 int statusCode =response.getStatusCode() ;
+	 
+	 Assert.assertEquals(statusCode, 200);
+	
+	}
+	
+	@Test
+	void test_2()
+	{
+		given().get("https://reqres.in/api/users?page=2")
+		.then()
+		.statusCode(200)
+//		.body("data.id[0]", equalTo(7));
+		.body("data.first_name[0]", equalTo("Michael"));
+		
+	}
+	
+
+}
